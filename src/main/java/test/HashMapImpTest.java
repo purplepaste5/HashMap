7c7900f4d6b1a75bce78b62ee3de1b9ed50a3fda
purplepaste5 <57466423+purplepaste5@users.noreply.github.com>
2022-10-13 13:29:29 +0100

    add basic tests

--- src/main/java/test/HashMapImpTest.java
+++ src/main/java/test/HashMapImpTest.java
@@ -1,6 +1,32 @@
+package test;
 
+import main.java.HashMap.HashMapImp;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HashMapImpTest {
 
-    
+    HashMapImp hashMapImp;
+
+    @BeforeEach
+    public void setup() {
+        hashMapImp = new HashMapImp();
+    }
+
+    @Test
+    public void testHashMapAddSingleObject() {
+        String theReturn = (String) hashMapImp.add("test", "testResult");
+
+        assertEquals("testResult", theReturn);
+    }
+
+    @Test
+    public void testHashMapSimpleGet() {
+        hashMapImp.add("test", "testResult");
+
+        assertEquals("testResult", hashMapImp.get("test"));
+    }
+
 }
